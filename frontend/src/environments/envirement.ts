56d74a10f0f79371b444ac43a614188ac748515a
vitalii.dubyk <vitalii.dubyk@avenga.com>
2024-09-17 00:55:41 +0300

    fix

--- frontend/src/environments/envirement.ts
+++ frontend/src/environments/envirement.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'https://backend-lb-698672478.eu-west-1.elb.amazonaws.com/api'
+    apiBaseUrl: 'http://backend-lb-698672478.eu-west-1.elb.amazonaws.com/api'
 };
