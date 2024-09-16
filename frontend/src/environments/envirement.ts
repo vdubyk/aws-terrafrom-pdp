b44ec586b1356668adb861a5c00b1117dabbfecd
vitalii.dubyk <vitalii.dubyk@avenga.com>
2024-09-17 00:07:42 +0300

    fix

--- frontend/src/environments/envirement.ts
+++ frontend/src/environments/envirement.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'http://local:8080/api'
+    apiBaseUrl: 'http://localhost:8080/api'
 };
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'http://loc:8080/api'
+    apiBaseUrl: 'http://localhost:8080/api'
 };
